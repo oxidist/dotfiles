910524cd12fc761604bb9af77f305f8ddde9f33b
OfficialOxide <best.ali2002@gmail.com>
2020-05-16 16:11:34 +0530

    i3, weather, zathura

--- scripts/scripts/weather.py
+++ scripts/scripts/weather.py
@@ -1,58 +1,8 @@
 #!/usr/bin/python
-import pyowm
+import requests
 
-class WeatherGetter():
-    # How to enter the argument city:
-    #   (your city),(your two letter country code)
-    #   for example, the argument for London would be:
-    #   "London,gb"
+location = "462002"
+dump = (requests.get('http://wttr.in/{}?format=j1'.format(location)).json())['current_condition'][0]
+temp = dump["FeelsLikeC"]
 
-    # How to enter the argument unit:
-    #   Enter a string, "C" or "F" for Celsius unit or Fahrenheit unit respectively.
-    def __init__(self, city, unit, api_key):
-        self.city = city
-        self.unit = unit
-        self.api_key = api_key # get api key from OWM after signing up
-
-    def get_weather_of_city(self):
-        owm = pyowm.OWM(self.api_key)
-        weather_in_city = owm.weather_at_place(self.city)
-        weather = weather_in_city.get_weather() # returns a Weather object, with
-        # the temperature, status, etc.
-        return weather
-
-    def get_temp_of_city(self, weather):
-        temp_in_kelvin = weather.get_temperature() # Returns a dict
-        return temp_in_kelvin
-
-    def get_status_of_city(self, weather):
-        status = weather.get_status() # for eg. sunny, clouds, etc.
-        return status
-    def output_the_weather(self, temp_in_kelvin, status):
-        psbl = {"Clouds":" ",
-                "Haze": " " ,
-                "Clear": " ",
-                "Rain": " ",
-                "Thunderstorm": " ",
-                "Drizzle": " ",
-                "Dust": " ",
-                "Mist": " "}
-
-        icon = psbl.get(status)
-
-        if self.unit == "C":
-            tmp = pyowm.utils.temputils.kelvin_to_celsius(temp_in_kelvin["temp"])
-
-        elif self.unit == "F":
-            tmp = pyowm.utils.temputils.kelvin_to_fahrenheit(temp_in_kelvin["temp"])
-
-        return "{}{}, {}°{}".format(icon, status, tmp, self.unit)
-
-
-reporter = WeatherGetter("Bhopal, IN","C","d2e1cb17172b8247f4eac5f53b458e3c")
-
-weather = reporter.get_weather_of_city()
-
-status = reporter.get_status_of_city(weather)
-temp = reporter.get_temp_of_city(weather)
-print (reporter.output_the_weather(temp, status))
+print("{}°C".format(temp))
